42f5384825f83772316aacc554fffefba0c92f68
Juan Ignacio Saa Hargous <juansaa@gmail.com>
2023-02-14 16:06:28 -0800

    Refactor Advent of code 2015 day 23

--- Advent of Code 2015/day23.py
+++ Advent of Code 2015/day23.py
@@ -3,46 +3,34 @@ instructions = [line.strip() for line in open("inputs/day23.in").readlines()]
 
 def execute(a, b):
     i = 0
+    registers = {"a": a, "b": b}
     while 0 <= i < len(instructions):
         op = instructions[i][:3]
         if op == "hlf":
-            if instructions[i][-1] == "a":
-                a //= 2
-            else:
-                b //= 2
+            registers[instructions[i][4]] //= 2
             i += 1
         elif op == "tpl":
-            if instructions[i][-1] == "a":
-                a *= 3
-            else:
-                b *= 3
+            registers[instructions[i][4]] *= 3
             i += 1
         elif op == "inc":
-            if instructions[i][-1] == "a":
-                a += 1
-            else:
-                b += 1
+            registers[instructions[i][4]] += 1
             i += 1
         elif op == "jmp":
             offset = int(instructions[i][4:])
             i += offset
         elif op == "jie":
             offset = int(instructions[i][7:])
-            if instructions[i][4] == "a" and a % 2 == 0:
-                i += offset
-            elif instructions[i][4] == "b" and b % 2 == 0:
+            if registers[instructions[i][4]] % 2 == 0:
                 i += offset
             else:
                 i += 1
         elif op == "jio":
             offset = int(instructions[i][7:])
-            if instructions[i][4] == "a" and a == 1:
-                i += offset
-            elif instructions[i][4] == "b" and b == 1:
+            if registers[instructions[i][4]] == 1:
                 i += offset
             else:
                 i += 1
-    return b
+    return registers["b"]
 
 
 print("Part 1:", execute(0, 0))
