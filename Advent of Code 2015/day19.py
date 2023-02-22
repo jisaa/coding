0fe97e816aed58793f68894e6d7128e263d6ce40
Juan Ignacio Saa Hargous <juansaa@gmail.com>
2023-02-22 10:26:54 -0800

    Advent of code 2015 day 19 part 2

--- Advent of Code 2015/day19.py
+++ Advent of Code 2015/day19.py
@@ -10,7 +10,7 @@ for line in open("inputs/day19.in"):
         medicine = line
 
 
-def distinct_replacements(start):
+def distinct_replacements(start, replacements):
     new_molecules = set()
     for i in range(len(start)):
         for k in replacements:
@@ -21,4 +21,28 @@ def distinct_replacements(start):
     return new_molecules
 
 
-print("Part 1:", len(distinct_replacements(medicine)))
+print("Part 1:", len(distinct_replacements(medicine, replacements)))
+
+# start from medicine, apply replacements backwards, end up with "e"
+q = [(0, medicine)]
+seen = set()
+
+reverse = {}
+for k in replacements:
+    for v in replacements[k]:
+        reverse[v] = [k]
+
+q = [(0, medicine)]
+seen = set()
+seen.add(medicine)
+while q:
+    # pick shortest molecule available
+    q.sort(key=lambda i: len(i[1]))
+    steps, molecule = q.pop(0)
+    for mutation in distinct_replacements(molecule, reverse):
+        if mutation == "e":
+            print("Part 2:", steps + 1)
+            exit()
+        if mutation not in seen:
+            q.append((steps + 1, mutation))
+            seen.add(mutation)
