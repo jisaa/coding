3faef111bf3e43ec99869bb73ca8887dd2e9a9ed
Juan Ignacio Saa Hargous <juansaa@gmail.com>
2023-02-22 09:29:20 -0800

    Refactor advent of code 2015 day 19 part 1

--- Advent of Code 2015/day19.py
+++ Advent of Code 2015/day19.py
@@ -1,5 +1,4 @@
 replacements = {}
-start = ""
 for line in open("inputs/day19.in"):
     line = line.strip()
     if " => " in line:
@@ -8,14 +7,18 @@ for line in open("inputs/day19.in"):
             replacements[a] = []
         replacements[a].append(b)
     else:
-        start = line
+        medicine = line
 
-new_molecules = set()
-for i in range(len(start)):
-    for k in replacements:
-        if k == start[i : i + len(k)]:
-            for b in replacements[k]:
-                t = start[:i] + b + start[i + len(k) :]
-                new_molecules.add(t)
 
-print("Part 1:", len(new_molecules))
+def distinct_replacements(start):
+    new_molecules = set()
+    for i in range(len(start)):
+        for k in replacements:
+            if k == start[i : i + len(k)]:
+                for b in replacements[k]:
+                    t = start[:i] + b + start[i + len(k) :]
+                    new_molecules.add(t)
+    return new_molecules
+
+
+print("Part 1:", len(distinct_replacements(medicine)))
