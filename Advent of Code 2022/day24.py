84fb0d22637ff20a0b5a93af1212f9e4071a4189
Juan Ignacio Saa Hargous <juansaa@gmail.com>
2023-02-22 17:15:20 -0800

    Solve part 1

--- Advent of Code 2022/day24.py
+++ Advent of Code 2022/day24.py
@@ -1,19 +1,18 @@
-f = open("p24.in")
+f = open("inputs/day24.in")
 
 grid = []
 for line in f.readlines():
-    grid.append(line.strip())
+    grid.append(line.strip()[1:-1])
+grid = grid[1:-1]
 
 storms = []
 for i in range(len(grid)):
     for j in range(len(grid[i])):
         if grid[i][j] in "><^v":
-            storms.append((i - 1, j - 1, grid[i][j]))
-
-end_a, end_b = len(grid) - 1, len(grid[-1]) - 1
+            storms.append((i, j, grid[i][j]))
 
 
-def are_storms_at(i, j, time):
+def are_storms_at(i, j, storms, time):
     wind = {
         ">": (0, 1),
         "<": (0, -1),
@@ -29,32 +28,32 @@ def are_storms_at(i, j, time):
     return False
 
 
+# Takes about 6 minutes to finish
 # pos_i, pos_j, time
 q = [(-1, 0, 0)]
-vueltas = 0
-while 1:
+best_time = 9999
+seen = set(q)
+while q:
+    # try the position closest to the exit (Manhattan distance)
     q.sort(key=lambda x: x[0] + x[1])
-    print(len(q), max(t[-1] for t in q), vueltas)
-    vueltas += 1
     a, b, time = q.pop()
     # can exit
     if a == len(grid) - 1 and b == len(grid[0]) - 1:
-        print("Part 1:", time + 1)
-        break
-    # can stay
-    if not are_storms_at(a, b, time + 1):
-        q.append((a, b, time + 1))
-    # can move right
-    if b + 1 < len(grid[a]) and not are_storms_at(a, b + 1, time + 1):
-        q.append((a, b + 1, time + 1))
-    # can move left
-    if b - 1 >= 0 and not are_storms_at(a, b - 1, time + 1):
-        q.append((a, b - 1, time + 1))
-    # can move down
-    if a + 1 < len(grid) and not are_storms_at(a + 1, b, time + 1):
-        q.append((a + 1, b, time + 1))
-    # can move up
-    if a - 1 >= 0 and not are_storms_at(a - 1, b, time + 1):
-        q.append((a - 1, b, time + 1))
+        if time + 1 < best_time:
+            best_time = time + 1
+            q = [t for t in q if t[2] < best_time]
+    # check all directions, including current position
+    for da, db in ((0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)):
+        x = a + da
+        y = b + db
+        if (
+            0 <= x < len(grid)
+            and 0 <= y < len(grid[x])
+            and not are_storms_at(x, y, storms, time + 1)
+            and (x, y, time + 1) not in seen
+            and time + 1 < best_time
+        ):
+            seen.add((x, y, time + 1))
+            q.append((x, y, time + 1))
 
-# 327 too low
+print("Part 1:", best_time)
