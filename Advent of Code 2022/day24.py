326e0a9c481bdfc7379f2841dc15a38bdac69ef5
Juan Ignacio Saa Hargous <juansaa@gmail.com>
2023-02-24 08:58:37 -0800

    Prune more paths, reduce running time in half

--- Advent of Code 2022/day24.py
+++ Advent of Code 2022/day24.py
@@ -1,17 +1,3 @@
-f = open("inputs/day24.in")
-
-grid = []
-for line in f.readlines():
-    grid.append(line.strip()[1:-1])
-grid = grid[1:-1]
-
-storms = []
-for i in range(len(grid)):
-    for j in range(len(grid[i])):
-        if grid[i][j] in "><^v":
-            storms.append((i, j, grid[i][j]))
-
-
 def are_storms_at(i, j, storms, time):
     wind = {
         ">": (0, 1),
@@ -28,32 +14,54 @@ def are_storms_at(i, j, storms, time):
     return False
 
 
-# Takes about 6 minutes to finish
-# pos_i, pos_j, time
-q = [(-1, 0, 0)]
-best_time = 9999
-seen = set(q)
-while q:
-    # try the position closest to the exit (Manhattan distance)
-    q.sort(key=lambda x: x[0] + x[1])
-    a, b, time = q.pop()
-    # can exit
-    if a == len(grid) - 1 and b == len(grid[0]) - 1:
-        if time + 1 < best_time:
-            best_time = time + 1
-            q = [t for t in q if t[2] < best_time]
-    # check all directions, including current position
-    for da, db in ((0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)):
-        x = a + da
-        y = b + db
-        if (
-            0 <= x < len(grid)
-            and 0 <= y < len(grid[x])
-            and not are_storms_at(x, y, storms, time + 1)
-            and (x, y, time + 1) not in seen
-            and time + 1 < best_time
-        ):
-            seen.add((x, y, time + 1))
-            q.append((x, y, time + 1))
+# Takes about 3 minutes to finish
+def get_best_time(storms, max_x, max_y, from_i, from_j, to_i, to_j):
+    # pos_i, pos_j, time
+    q = [(from_i, from_j, 0)]
+    best_time = 9999
+    seen = set(q)
+    while q:
+        # try the position closest to the exit (Manhattan distance)
+        q.sort(key=lambda x: x[0] + x[1])
+        a, b, time = q.pop()
+        # can exit
+        if a == to_i and b == to_j:
+            if time + 1 < best_time:
+                best_time = time + 1
+                q = [t for t in q if t[2] < best_time]
+        # check all directions, including current position
+        for da, db in ((0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)):
+            x = a + da
+            y = b + db
+            if (
+                0 <= x < max_x
+                and 0 <= y < max_y
+                and max_x - x + max_y - y < best_time - time
+                and time + 1 < best_time
+                and (x, y, time + 1) not in seen
+                and not are_storms_at(x, y, storms, time + 1)
+            ):
+                seen.add((x, y, time + 1))
+                q.append((x, y, time + 1))
+    return best_time
 
-print("Part 1:", best_time)
+
+f = open("inputs/day24.in")
+
+grid = []
+for line in f.readlines():
+    grid.append(line.strip()[1:-1])
+grid = grid[1:-1]
+
+storms = []
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        if grid[i][j] in "><^v":
+            storms.append((i, j, grid[i][j]))
+
+print(
+    "Part 1:",
+    get_best_time(
+        storms, len(grid), len(grid[0]), -1, 0, len(grid) - 1, len(grid[0]) - 1
+    ),
+)
