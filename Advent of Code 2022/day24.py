f9226a9081cea526b7305705258a633e869cb624
Juan Ignacio Saa Hargous <juansaa@gmail.com>
2023-02-24 10:43:09 -0800

    Part 2

--- Advent of Code 2022/day24.py
+++ Advent of Code 2022/day24.py
@@ -15,19 +15,20 @@ def are_storms_at(i, j, storms, time):
 
 
 # Takes about 3 minutes to finish
-def get_best_time(storms, max_x, max_y, from_i, from_j, to_i, to_j):
+def get_best_time(storms, max_x, max_y, from_i, from_j, to_i, to_j, start_time=0):
     # pos_i, pos_j, time
-    q = [(from_i, from_j, 0)]
+    q = [(from_i, from_j, start_time)]
     best_time = 9999
     seen = set(q)
     while q:
         # try the position closest to the exit (Manhattan distance)
-        q.sort(key=lambda x: x[0] + x[1])
-        a, b, time = q.pop()
+        q.sort(key=lambda x: abs(to_i - x[0]) + abs(to_j - x[1]))
+        a, b, time = q.pop(0)
         # can exit
         if a == to_i and b == to_j:
             if time + 1 < best_time:
                 best_time = time + 1
+                print(best_time)
                 q = [t for t in q if t[2] < best_time]
         # check all directions, including current position
         for da, db in ((0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)):
@@ -36,7 +37,7 @@ def get_best_time(storms, max_x, max_y, from_i, from_j, to_i, to_j):
             if (
                 0 <= x < max_x
                 and 0 <= y < max_y
-                and max_x - x + max_y - y < best_time - time
+                and abs(x - to_i) + abs(y - to_j) < best_time - time
                 and time + 1 < best_time
                 and (x, y, time + 1) not in seen
                 and not are_storms_at(x, y, storms, time + 1)
@@ -59,9 +60,37 @@ for i in range(len(grid)):
         if grid[i][j] in "><^v":
             storms.append((i, j, grid[i][j]))
 
-print(
-    "Part 1:",
-    get_best_time(
-        storms, len(grid), len(grid[0]), -1, 0, len(grid) - 1, len(grid[0]) - 1
-    ),
+part_1_time = get_best_time(
+    storms,
+    len(grid),
+    len(grid[0]),
+    -1,
+    0,
+    len(grid) - 1,
+    len(grid[0]) - 1,
 )
+print("Part 1:", part_1_time)
+
+back_to_start_time = get_best_time(
+    storms,
+    len(grid),
+    len(grid[0]),
+    len(grid),
+    len(grid[0]) - 1,
+    0,
+    0,
+    part_1_time,
+)
+print("Back to start:", back_to_start_time)
+final_time = get_best_time(
+    storms,
+    len(grid),
+    len(grid[0]),
+    -1,
+    0,
+    len(grid) - 1,
+    len(grid[0]) - 1,
+    back_to_start_time,
+)
+
+print("Part 2:", final_time)
